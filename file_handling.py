19a46bea717e0338681e7e9f0360bac5b2ad4f64
poovarasan4321 <poovarasan2927@gmail.com>
2026-03-26 08:25:29 +0530

    "adeed new files"

--- file_handling.py
+++ file_handling.py
@@ -14,17 +14,17 @@
 
 
 # when you want specific lines only
-with open("python.py", 'r') as e:
-    i = 0
-    # if we are not create ouside it's throe error name not fount that's why
-    line = e.readline()
-    while line:
-        line = e.readline()
-        print(line.split(","))
-        # if we want to exact particullar thing means    now its a list  if ' word' in  list print(line)
-        if i % 100 == 0:
-            op = input("you want more :")
-            if op.lower() == 'no':
-                print("stop")
-                break
-        i += 1
+# with open("python.py", 'r') as e:
+#     i = 0
+#     # if we are not create ouside it's throe error name not fount that's why
+#     line = e.readline()
+#     while line:
+#         line = e.readline()
+#         print(line.split(","))
+#         # if we want to exact particullar thing means    now its a list  if ' word' in  list print(line)
+#         if i % 100 == 0:
+#             op = input("you want more :")
+#             if op.lower() == 'no':
+#                 print("stop")
+#                 break
+#         i += 1
